be774ee87cc11144dd72d5a36b9747f5fe8d9b83
Decode <amitkumar696945@gmail.com>
2024-01-14 18:05:09 +0530

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-
+import Navbar from './Components/Navbar/Navbar.jsx'
+import {BrowserRouter, Routes, Route}  from 'react-router-dom';
+import Shop from './Pages/Shop.jsx';
+import ShopCategory from './Pages/ShopCategory.jsx';
 function App() {
   
   return (
     <>
+    <BrowserRouter>
       <Navbar/>
+      <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+      <Route path='/womens' element={<ShopCategory/>}/>
+      <Route path='/kids' element={<ShopCategory/>}/>
+      
+      </Routes>
+      
+      </BrowserRouter>
     
     </>
   )
