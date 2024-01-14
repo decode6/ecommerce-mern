0c990f0ef09cf1332d3b5419dda4168e3ac2f007
Decode <amitkumar696945@gmail.com>
2024-01-14 22:31:43 +0530

    map function to fetch data

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Popular.css'
+import data_product from '../Assets/data'
 
 const Popular = () => {
   return (
-    <div className='popular'>Popular</div>
+    <div className='popular'>
+        <h1>POPULAR IN WOMEN</h1>
+        <hr />
+        <div className="popular-item">
+            
+        </div>
+        </div>
   )
 }
 
