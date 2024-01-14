9e3835070f1e25034d016de1cb856f1de91c55ce
Decode <amitkumar696945@gmail.com>
2024-01-14 18:15:07 +0530

    routes setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-
-import './App.css'
-import Navbar from './Components/Navbar/Navbar.jsx'
-import {BrowserRouter, Routes, Route}  from 'react-router-dom';
-import Shop from './Pages/Shop.jsx';
-import ShopCategory from './Pages/ShopCategory.jsx';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop.jsx";
+import ShopCategory from "./Pages/ShopCategory.jsx";
+import Product from "./Pages/Product.jsx";
+import Cart from "./Pages/Cart.jsx";
+import LoginSignup from "./Pages/LoginSignup.jsx";
 function App() {
-  
   return (
     <>
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-      <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-      <Route path='/womens' element={<ShopCategory category="women"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-      <Route path='/product' element={<ShopCategory category="kid"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-      
-      </Routes>
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>} />
+          </Route>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/login" element={<LoginSignup/>}/>
+        </Routes>
       </BrowserRouter>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
