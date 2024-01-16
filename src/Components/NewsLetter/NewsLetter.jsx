acc2e6299be7bb4138f931d710656bdefaeeaae7
Decode <amitkumar696945@gmail.com>
2024-01-16 21:06:19 +0530

    newsletter component updated

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -5,6 +5,10 @@ const NewsLetter = () => {
     <div className='newsletter'>
         <h1>Get Exclusive Offers on Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your Email id' />
+            <button>Subscribe</button>
+        </div>
     </div>
   )
 }
