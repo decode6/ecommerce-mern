88e610c910c4eeb463d75760471f7d8a3c44c9d0
Decode <amitkumar696945@gmail.com>
2024-01-16 21:53:01 +0530

    context api

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext } from "react";
+import all_product from '../Components/Assets/all_product'
+
+
+export const ShopContext= createContext(null);
+
+
+const ShopContext =(props)=>{
+
+
+    const contextValue ={all_product};
+    return(
+        
+    )
+}
