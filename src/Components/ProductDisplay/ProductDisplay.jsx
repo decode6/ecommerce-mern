2d5808bbbb92143741b86e9c265dca1b270c860e
Decode <amitkumar696945@gmail.com>
2024-01-17 23:04:03 +0530

    product page structure completed

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -30,6 +30,27 @@ const ProductDisplay = (props) => {
             <img src={star_dull_icon} alt="." />
             <p>{133}</p>
            </div>
+           <div className="productdisplay-right-prices">
+             <div className="productdisplay-right-price-old">${product.old_price}</div>
+             <div className='productdisplay-right-price-old'>${product.new_price} </div>
+           </div>
+           <div className="productdisplay-right-description">
+            A lightweight,usually knitted,pullover shirt, close fitting and a round neckline 
+
+           </div>
+           <div className="productdisplay-right-size">
+            <h1>Select Size</h1>
+            <div className="productdisplay-right-size">
+                <div>Small</div>
+                <div>Medium</div>
+                <div>Large</div>
+                <div>Xl</div>
+                <div>XXL</div>
+            </div>
+           </div>
+         <button>ADD TO CART</button>
+         <p className="productdisplay-right-category"><span>Category :</span> Women, T-Shirt , Crop Top</p>
+         <p className="productdisplay-right-category"><span>Tags :</span> Modern, Latest</p>
         </div>
       
     </div>
