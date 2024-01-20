9cc0aa1fbcc48ecc0a33112bf73811ccdc8ad09b
Decode <amitkumar696945@gmail.com>
2024-01-20 20:31:02 +0530

    breadcrum

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Breadcrums.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
-
+//breadcrum 
 const Breadcrum = (props) => {
 
     const {product} = props;
