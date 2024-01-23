1ae0ccd5a8d1610def69894ca844ce4f995ef3f0
Decode <amitkumar696945@gmail.com>
2024-01-23 21:04:34 +0530

    productdisplay

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -3,7 +3,7 @@ import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
 
-
+//Productdisplay
 const ProductDisplay = (props) => {
     const {product} = props;
   
